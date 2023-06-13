5ef222cd4358d2d4734418f9208f0381b4b86f48
Angeline90 <angeline.gauron@gmail.com>
2023-06-13 16:55:44 +0200

    props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,33 +1,22 @@
 import React from 'react';
 
-function PokemonCard() {
-  const pokemon = pokemonList[0];
+function PokemonCard(props) {
+    console.log(props);
+  const pokemon = props.pokemon;
 
   return (
     <figure>
-      {pokemon.imgSrc && (
+      {pokemon.imgSrc ? (
         <img
           src={pokemon.imgSrc}
           alt={pokemon.name}
         />
-      )}
-      {!pokemon.imgSrc && (
+      ) : (
         <p>???</p>
       )}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
 export default PokemonCard;
